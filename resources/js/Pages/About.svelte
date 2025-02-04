e8c675a25b24528eb474e5d49b34a4ddb3650ba6
Allen <allenjianggo@gmail.com>
2025-02-04 14:19:18 +0800

    build: upgrade to tailwind css 4

--- resources/js/Pages/About.svelte
+++ resources/js/Pages/About.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import Layout from "../Components/Layout.svelte";
+    import Layout from "#components/Layout.svelte";
 
     let { title } = $props();
 </script>
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [
@@ -10,5 +11,6 @@ export default defineConfig({
             refresh: true,
         }),
         svelte(),
+        tailwindcss(),
     ],
 });
