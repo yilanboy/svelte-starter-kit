b5ee36dbcd0078ad7b0398ba70cf3f7d00e05ee7
Allen <allenjianggo@gmail.com>
2025-06-27 15:32:58 +0800

    refactor: extract settings navigation into reusable `Navigation` component

---
+++ resources/js/Pages/settings/partials/Navigation.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import { inertia, page } from "@inertiajs/svelte";
+  import ProfileController from "@/actions/App/Http/Controllers/ProfileController";
+  import PasswordController from "@/actions/App/Http/Controllers/PasswordController";
+</script>
+
+<ul class="flex flex-col gap-1">
+  <li>
+    <a
+      use:inertia
+      href={ProfileController.edit().url}
+      class={{
+        "bg-zinc-100": $page.url === "/settings/profile",
+        "hover:bg-zinc-100": $page.url !== "/settings/profile",
+        "block rounded-md px-4 py-2 ": true,
+      }}
+    >
+      Profile
+    </a>
+  </li>
+  <li>
+    <a
+      use:inertia
+      href={PasswordController.edit().url}
+      class={{
+        "bg-zinc-100": $page.url === "/settings/password",
+        "hover:bg-zinc-100": $page.url !== "/settings/password",
+        "block rounded-md px-4 py-2 ": true,
+      }}
+    >
+      Password
+    </a>
+  </li>
+</ul>
--- resources/js/Pages/settings/password/Page.svelte
+++ resources/js/Pages/settings/password/Page.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
   import LayoutMain from "@/components/layouts/main/LayoutMain.svelte";
-  import ProfileController from "@/actions/App/Http/Controllers/ProfileController";
-  import PasswordController from "@/actions/App/Http/Controllers/PasswordController";
-  import { inertia, page } from "@inertiajs/svelte";
+  import Navigation from "@/pages/settings/partials/Navigation.svelte";
 
   interface Props {
     title: string;
@@ -27,34 +25,7 @@
         </div>
 
         <div class="col-span-1">
-          <ul class="flex flex-col gap-1">
-            <li>
-              <a
-                use:inertia
-                href={ProfileController.edit().url}
-                class={{
-                  "bg-zinc-100": $page.url === "/settings/profile",
-                  "hover:bg-zinc-100": $page.url !== "/settings/profile",
-                  "block rounded-md px-4 py-2 ": true,
-                }}
-              >
-                Profile
-              </a>
-            </li>
-            <li>
-              <a
-                use:inertia
-                href={PasswordController.edit().url}
-                class={{
-                  "bg-zinc-100": $page.url === "/settings/password",
-                  "hover:bg-zinc-100": $page.url !== "/settings/password",
-                  "block rounded-md px-4 py-2 ": true,
-                }}
-              >
-                Password
-              </a>
-            </li>
-          </ul>
+          <Navigation />
         </div>
 
         <div class="col-span-3"></div>
