ef5db4c2e3d68fe4b4ffbcd90743553f0da2b5f8
Allen <allenjianggo@gmail.com>
2025-07-14 22:37:46 +0800

    feat: add password update form to settings page

--- resources/js/Pages/settings/password/Page.svelte
+++ resources/js/Pages/settings/password/Page.svelte
@@ -1,12 +1,19 @@
 <script lang="ts">
   import LayoutMain from "@/components/layouts/main/LayoutMain.svelte";
   import Navigation from "@/pages/settings/partials/Navigation.svelte";
+  import { useForm } from "@inertiajs/svelte";
 
   interface Props {
     title: string;
   }
 
   let { title }: Props = $props();
+
+  const form = useForm({
+    current_password: "",
+    password: "",
+    password_confirmation: "",
+  });
 </script>
 
 <svelte:head>
@@ -28,7 +35,77 @@
           <Navigation />
         </div>
 
-        <div class="col-span-3"></div>
+        <div class="col-span-3">
+          <div class="sm:w-full sm:max-w-sm">
+            <form class="space-y-6">
+              <div>
+                <label
+                  for="current_password"
+                  class="block text-base font-medium text-gray-900"
+                >
+                  Current password
+                </label>
+                <div class="mt-2">
+                  <input
+                    bind:value={$form.current_password}
+                    type="password"
+                    id="current_password"
+                    autocomplete="current-password"
+                    required
+                    class="block w-full rounded-md bg-zinc-50 px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-blue-600"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <label
+                  for="password"
+                  class="block text-base font-medium text-gray-900"
+                >
+                  New password
+                </label>
+                <div class="mt-2">
+                  <input
+                    bind:value={$form.password}
+                    type="password"
+                    id="password"
+                    autocomplete="new-password"
+                    required
+                    class="block w-full rounded-md bg-zinc-50 px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-blue-600"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <label
+                  for="password_confirmation"
+                  class="block text-base font-medium text-gray-900"
+                >
+                  Confirm password
+                </label>
+                <div class="mt-2">
+                  <input
+                    bind:value={$form.password_confirmation}
+                    type="password"
+                    id="password_confirmation"
+                    autocomplete="new-password"
+                    required
+                    class="block w-full rounded-md bg-zinc-50 px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-blue-600"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <button
+                  type="submit"
+                  class="flex cursor-pointer justify-center rounded-md bg-blue-600 px-3 py-1.5 text-base font-semibold text-zinc-50 shadow-xs hover:bg-blue-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+                >
+                  Save password
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
     </div>
   </main>
