a1dd0d68477ca4518ebad24c2f759c74a73fff24
allenjiang <allenjianggo@gmail.com>
2025-12-14 20:14:12 +0800

    refactor: simplify `InputWithLabel` component by extending `HTMLInputAttributes` and spreading props

--- resources/js/components/forms/InputWithLabel.svelte
+++ resources/js/components/forms/InputWithLabel.svelte
@@ -1,22 +1,16 @@
 <script lang="ts">
-  import type { FullAutoFill } from "svelte/elements";
+  import type { HTMLInputAttributes } from "svelte/elements";
 
-  interface Props {
+  interface Props extends HTMLInputAttributes {
     label: string;
     id: string;
-    name: string;
-    type?: string;
-    required?: boolean;
-    autocomplete?: FullAutoFill;
   }
 
   let {
     label,
     id,
-    name,
     type = "text",
-    required = false,
-    autocomplete,
+    ...rest
   }: Props = $props();
 </script>
 
@@ -26,11 +20,9 @@
   </label>
   <div class="mt-2">
     <input
-      {name}
-      {type}
       {id}
-      {required}
-      {autocomplete}
+      {type}
+      {...rest}
       class="block w-full rounded-md bg-zinc-50 px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-blue-600"
     />
   </div>
